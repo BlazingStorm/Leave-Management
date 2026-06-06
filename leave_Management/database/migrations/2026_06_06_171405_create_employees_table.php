388bfa596f76b7a46661d7c651006b6ab9a77793
shreedharsan <717822f155@kce.ac.in>
2026-06-06 22:55:21 +0530

    Project schema

---
+++ leave_Management/database/migrations/2026_06_06_171405_create_employees_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
+            $table->string('employee_id')->unique();
+            $table->string('name');
+
+            $table->string('mobile');
+            $table->string('department');
+            $table->string('designation');
+            $table->unsignedBigInteger('manager_id')->nullable();
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employee');
+    }
+};
